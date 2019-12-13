8be4207bbe7b4d2769723c82fa2abc82bb4ea50f
Antonio Buedo <sales-engineering@bitpay.com>
2019-12-13 14:31:03 +0100

    - Sub-version upgrade - Added ERC_20 support

--- src/main/java/com/bitpay/sdk_light/Client.java
+++ src/main/java/com/bitpay/sdk_light/Client.java
@@ -28,9 +28,9 @@ import java.util.*;
 
 /**
  * @author Antonio Buedo
- * @version 1.3.1912
+ * @version 1.4.1912
  * See bitpay.com/api for more information.
- * date 05.12.2019
+ * date 13.12.2019
  */
 
 public class Client {
